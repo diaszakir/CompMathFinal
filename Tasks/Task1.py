beda644157abc1f07cb5cd8b53d22ebea03a0d32
diaszakir <mailforstudy84@gmail.com>
2025-02-10 11:28:33 +0500

    new files

---
+++ Tasks/Task1.py
@@ -0,0 +1,5 @@
+# Task 1: Graphical Method and Absolute Error
+# Problem: Plot the graph of f(x)=cos(x)−x in the range x∈[0,1]. Use the graph to find an approximate
+# root and calculate the absolute error compared to the root found using a numerical method.
+
+import numpy as np
